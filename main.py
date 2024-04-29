8c4473da44aed8970e9ba975d703b4158929eca1
Corban <126821942+CorbanBurnett@users.noreply.github.com>
2024-04-29 00:05:50 +0000

    finnished pizza names

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+'''i am Creating The menu then i am adding in $8.50 pizzas then im adding in the $13.50
+pizzas for the pizza resturant called "Burnt"'''
 class PizzaOrderingSystem:
   MENU = {
-    "": 8.50,
-    "": 8.50,
-    "": 8.50,
-    "": 8.50,
-    "": 8.50,
-    "": 8.50,
-    "": 8.50,
+    "Peperoni": 8.50,
+    "Cheesy Garlic": 8.50,
+    "Ham and Cheese": 8.50,
+    "Beef and Onion": 8.50,
+    "Hawaiian": 8.50,
+    "Bacon & Aioli": 8.50,
+    "Classic Veggie": 8.50,
     "Greasy Grove": 13.50,
-    "": 13.50,
-    "": 13.50,
-    "": 13.50,
-    "": 13.50,
-    "": 13.50,
-    "": 13.50,
+    "Burnt Meaty Meatlovers": 13.50,
+    "Burnt Sausage": 13.50,
+    "Burnt Veggie": 13.50,
+    "Pleasent Plazza": 13.50,
+    "3500HP GTR R35": 13.50,
+    "ScatPack Hellcat": 13.50,
   }
